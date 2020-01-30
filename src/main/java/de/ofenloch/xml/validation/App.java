5739b813b54dfdd4a52f15e95cce3e74e158929c
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-30 22:33:02 +0100

    validate file data/students.xml against data/students.xsd

--- src/main/java/de/ofenloch/xml/validation/App.java
+++ src/main/java/de/ofenloch/xml/validation/App.java
@@ -4,10 +4,9 @@ package de.ofenloch.xml.validation;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println( "students.xml validates against students.xsd " + 
+            xmlvalidator.validateAgainstXSD("./data/students.xml", "./data/students.xsd" ));
     }
 }
