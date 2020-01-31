e4859e41c13d1c055916e03b5bf116af603a1e17
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-31 19:34:17 +0100

    format source, no changes in code

--- src/main/java/de/ofenloch/xml/validation/ResourceResolver.java
+++ src/main/java/de/ofenloch/xml/validation/ResourceResolver.java
@@ -6,18 +6,18 @@ import java.io.InputStream;
 import org.w3c.dom.ls.LSInput;
 import org.w3c.dom.ls.LSResourceResolver;
 
-
 public class ResourceResolver implements LSResourceResolver {
 
-  public static String XSD_DIRECTORY = "/home/ofenloch/workspaces/jee/xml-validation/data/OfficeOpenXML-XMLSchema-Transitional/";
+  public static String DATA_DIRECTORY = "/home/ofenloch/workspaces/jee/xml-validation/data/";
+  public static String XSD_DIRECTORY = "/home/ofenloch/workspaces/jee/xml-validation/data/OfficeOpenXML-XMLSchema/";
 
   public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
 
-  // type - The type of the resource being resolved. For XML [XML 1.0] resources (i.e. entities), applications must use the value "http://www.w3.org/TR/REC-xml". For XML Schema [XML Schema Part 1] , applications must use the value "http://www.w3.org/2001/XMLSchema". Other types of resources are outside the scope of this specification and therefore should recommend an absolute URI in order to use this method.
-  // namespaceURI - The namespace of the resource being resolved, e.g. the target namespace of the XML Schema [XML Schema Part 1] when resolving XML Schema resources.
-  // publicId - The public identifier of the external entity being referenced, or null if no public identifier was supplied or if the resource is not an entity.
-  // systemId - The system identifier, a URI reference [IETF RFC 2396], of the external resource being referenced, or null if no system identifier was supplied.
-  // baseURI - The absolute base URI of the resource being parsed, or null if there is no base URI.
+    // type - The type of the resource being resolved. For XML [XML 1.0] resources (i.e. entities), applications must use the value "http://www.w3.org/TR/REC-xml". For XML Schema [XML Schema Part 1] , applications must use the value "http://www.w3.org/2001/XMLSchema". Other types of resources are outside the scope of this specification and therefore should recommend an absolute URI in order to use this method.
+    // namespaceURI - The namespace of the resource being resolved, e.g. the target namespace of the XML Schema [XML Schema Part 1] when resolving XML Schema resources.
+    // publicId - The public identifier of the external entity being referenced, or null if no public identifier was supplied or if the resource is not an entity.
+    // systemId - The system identifier, a URI reference [IETF RFC 2396], of the external resource being referenced, or null if no system identifier was supplied.
+    // baseURI - The absolute base URI of the resource being parsed, or null if there is no base URI.
 
     // note: in this sample, the XSD's are expected to be in XSD_DIRECTORY
     try {
@@ -27,7 +27,7 @@ public class ResourceResolver implements LSResourceResolver {
       System.out.println("           publicId: " + publicId);
       System.out.println("            baseURI: " + baseURI);
 
-      if (systemId!=null) {
+      if (systemId != null) {
         InputStream resourceAsStream = new FileInputStream(XSD_DIRECTORY + systemId);
         System.out.println(" ---- found file \"" + XSD_DIRECTORY + systemId + "\" ...");
         return new Input(publicId, systemId, resourceAsStream);
