fc600e14a95ed4d6afaab124c6d82151e9d45580
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-31 07:31:04 +0100

    fix file name for xml/CustomerDetails02-Invalid.xml, print exceptions and messages

--- src/main/java/de/ofenloch/xml/validation/App.java
+++ src/main/java/de/ofenloch/xml/validation/App.java
@@ -25,25 +25,32 @@ public class App {
                 System.out.println("No Exceptions thrown. CustomerDetails01.xml is valid!");
             } catch (Exception e) {
                 System.out.println("Exceptions thrown. CustomerDetails01.xml is invalid!");
+                System.out.println(e.getMessage());
+                e.printStackTrace();
             }
             try {
                 // Validates Purchase01.xml
                 schema.newValidator().validate(new StreamSource(
                         new File(xmlvalidator.class.getClassLoader().getResource("xml/Purchase01.xml").getFile())));
                 System.out.println("No Exceptions thrown. Purchase01.xml is valid!");
-
             } catch (Exception e) {
                 System.out.println("Exceptions thrown. Purchase01.xml is invalid!");
+                System.out.println(e.getMessage());
+                e.printStackTrace();
             }
             try {
                 // Validates CustomerDetails01.xml
                 schema.newValidator().validate(new StreamSource(new File(xmlvalidator.class.getClassLoader()
-                        .getResource("xml/CustomerDetails02-invalid.xml").getFile())));
-                System.out.println("No Exceptions thrown. CustomerDetails02-invalid.xml is valid!");
+                        .getResource("xml/CustomerDetails02-Invalid.xml").getFile())));
+                System.out.println("No Exceptions thrown. CustomerDetails02-Invalid.xml is valid!");
             } catch (Exception e) {
-                System.out.println("Exceptions thrown. CustomerDetails02-invalid.xml is invalid!");
+                System.out.println("Exceptions thrown. CustomerDetails02-Invalid.xml is invalid!");
+                System.out.println(e.getMessage());
+                e.printStackTrace();
             }
         } catch (Exception ex) {
+            System.out.println("Exception thrown:");
+            System.out.println(ex.getMessage());
             ex.printStackTrace();
         }
     } // public static void main(String[] args)
