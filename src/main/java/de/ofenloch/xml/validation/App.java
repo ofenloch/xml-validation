6d274d276766840d1a1db0972e7c123a1684645d
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-31 07:24:35 +0100

    add validation of files, taken from https://github.com/Turreta/Validate-XML-against-an-XSD-that-imports-other-XSDs

--- src/main/java/de/ofenloch/xml/validation/App.java
+++ src/main/java/de/ofenloch/xml/validation/App.java
@@ -1,12 +1,50 @@
 package de.ofenloch.xml.validation;
 
+import java.io.File;
+
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println( "students.xml validates against students.xsd " + 
-            xmlvalidator.validateAgainstXSD("./data/students.xml", "./data/students.xsd" ));
-    }
+        System.out.println("students.xml validates against students.xsd "
+                + xmlvalidator.validateAgainstXSD("./data/students.xml", "./data/students.xsd"));
+
+        try {
+            // the following code was "stolen" from 
+            // https://github.com/Turreta/Validate-XML-against-an-XSD-that-imports-other-XSDs
+            Schema schema = xmlvalidator.getSchemaToValidateAgainst();
+            try {
+                // Validates CustomerDetails01.xml
+                schema.newValidator().validate(new StreamSource(new File(
+                        xmlvalidator.class.getClassLoader().getResource("xml/CustomerDetails01.xml").getFile())));
+                System.out.println("No Exceptions thrown. CustomerDetails01.xml is valid!");
+            } catch (Exception e) {
+                System.out.println("Exceptions thrown. CustomerDetails01.xml is invalid!");
+            }
+            try {
+                // Validates Purchase01.xml
+                schema.newValidator().validate(new StreamSource(
+                        new File(xmlvalidator.class.getClassLoader().getResource("xml/Purchase01.xml").getFile())));
+                System.out.println("No Exceptions thrown. Purchase01.xml is valid!");
+
+            } catch (Exception e) {
+                System.out.println("Exceptions thrown. Purchase01.xml is invalid!");
+            }
+            try {
+                // Validates CustomerDetails01.xml
+                schema.newValidator().validate(new StreamSource(new File(xmlvalidator.class.getClassLoader()
+                        .getResource("xml/CustomerDetails02-invalid.xml").getFile())));
+                System.out.println("No Exceptions thrown. CustomerDetails02-invalid.xml is valid!");
+            } catch (Exception e) {
+                System.out.println("Exceptions thrown. CustomerDetails02-invalid.xml is invalid!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    } // public static void main(String[] args)
 }
