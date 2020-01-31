ee287f8d65fe3651336d036927ae0d9e268bfeb2
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-31 07:24:07 +0100

    add method getSchemaToValidateAgainst

--- src/main/java/de/ofenloch/xml/validation/xmlvalidator.java
+++ src/main/java/de/ofenloch/xml/validation/xmlvalidator.java
@@ -1,4 +1,5 @@
 package de.ofenloch.xml.validation;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -13,14 +14,15 @@ public class xmlvalidator {
 
     static boolean validateAgainstXSD(final String xmlFileName, final String xsdFileName) {
         try {
-        InputStream xml = new FileInputStream(new File(xmlFileName));
-        InputStream xsd = new FileInputStream(new File(xsdFileName));
-        return validateAgainstXSD(xml, xsd);
+            InputStream xml = new FileInputStream(new File(xmlFileName));
+            InputStream xsd = new FileInputStream(new File(xsdFileName));
+            return validateAgainstXSD(xml, xsd);
         } catch (Exception ex) {
             ex.printStackTrace();
             return false;
         }
-    } // static boolean validateAgainstXSD(final String xmlFileName, final String xsdFileName)
+    } // static boolean validateAgainstXSD(final String xmlFileName, final String
+      // xsdFileName)
 
     static boolean validateAgainstXSD(InputStream xml, InputStream xsd) {
         try {
@@ -34,5 +36,20 @@ public class xmlvalidator {
             return false;
         }
     } // static boolean validateAgainstXSD(InputStream xml, InputStream xsd)
+
+    /**
+     * this is "stolen" from https://github.com/Turreta/Validate-XML-against-an-XSD-that-imports-other-XSDs
+     * @return
+     * @throws Exception
+     */
+    static public Schema getSchemaToValidateAgainst() throws Exception {
+        // We reference the main XSD that imports other XSD files
+        String mainXsdStr = "myxsds/Main.xsd";
+
+        SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+        Schema schema = schemaFactory.newSchema(xmlvalidator.class.getClassLoader().getResource(mainXsdStr));
+
+        return schema;
+    } // private static Schema getSchemaToValidateAgainst() throws Exception
 
 } // public class xmlvalidator
