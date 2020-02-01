8521e1bdb532496dc641d8303d9393902408c2fb
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-02-01 13:05:14 +0100

    some small modifications

--- src/test/java/de/ofenloch/xml/validation/AppTest.java
+++ src/test/java/de/ofenloch/xml/validation/AppTest.java
@@ -25,38 +25,43 @@ public class AppTest {
     public void testValidateAgainstXsd() {
         // the following code was "stolen" from
         // https://github.com/Turreta/Validate-XML-against-an-XSD-that-imports-other-XSDs
+        String fileName = "";
         try {
+            fileName = "xml/CustomerDetails01.xml";
             Schema schema = xmlvalidator.getSchemaToValidateAgainst();
             // Validates CustomerDetails01.xml
             schema.newValidator().validate(new StreamSource(
-                    new File(xmlvalidator.class.getClassLoader().getResource("xml/CustomerDetails01.xml").getFile())));
-            System.out.println("No Exceptions thrown. CustomerDetails01.xml is valid!");
+                    new File(xmlvalidator.class.getClassLoader().getResource(fileName).getFile())));
+            System.out.println("No Exceptions thrown. " + fileName + " is valid!");
         } catch (Exception e) {
-            System.out.println("Exceptions thrown. CustomerDetails01.xml is invalid!");
+            System.out.println("Exceptions thrown. " + fileName + " is invalid!");
             System.out.println(e.getMessage());
-            e.printStackTrace();
+            //e.printStackTrace();
         }
         try {
+            fileName = "xml/Purchase01.xml";
             Schema schema = xmlvalidator.getSchemaToValidateAgainst();
             // Validates Purchase01.xml
             schema.newValidator().validate(new StreamSource(
-                    new File(xmlvalidator.class.getClassLoader().getResource("xml/Purchase01.xml").getFile())));
-            System.out.println("No Exceptions thrown. Purchase01.xml is valid!");
+                    new File(xmlvalidator.class.getClassLoader().getResource(fileName).getFile())));
+                    System.out.println("No Exceptions thrown. " + fileName + " is valid!");
         } catch (Exception e) {
-            System.out.println("Exceptions thrown. Purchase01.xml is invalid!");
+            System.out.println("Exceptions thrown. " + fileName + " is invalid!");
             System.out.println(e.getMessage());
-            e.printStackTrace();
+            //e.printStackTrace();
         }
+        // This one's supposed to fail:
         try {
+            fileName = "xml/CustomerDetails02-Invalid.xml";
             Schema schema = xmlvalidator.getSchemaToValidateAgainst();
             // Validates CustomerDetails01.xml
             schema.newValidator().validate(new StreamSource(new File(
-                    xmlvalidator.class.getClassLoader().getResource("xml/CustomerDetails02-Invalid.xml").getFile())));
-            System.out.println("No Exceptions thrown. CustomerDetails02-Invalid.xml is valid!");
+                    xmlvalidator.class.getClassLoader().getResource(fileName).getFile())));
+                    System.out.println("No Exceptions thrown. " + fileName + " is valid!");
         } catch (Exception e) {
-            System.out.println("Exceptions thrown. CustomerDetails02-Invalid.xml is invalid!");
+            System.out.println("Exceptions thrown. " + fileName + " is invalid!");
             System.out.println(e.getMessage());
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     } // public void testValidateAgainstXsd()
 } // public class AppTest
